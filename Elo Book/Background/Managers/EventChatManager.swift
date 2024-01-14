022a856bd60a5517f6af6c739a1daad217e1669f
Jed <jedkutai@gmail.com>
2024-01-14 04:26:48 -0500

    added event chatting

---
+++ Elo Book/Background/Managers/EventChatManager.swift
@@ -0,0 +1,8 @@
+//
+//  EventChatManager.swift
+//  Elo Book
+//
+//  Created by Jed Kutai on 1/14/24.
+//
+
+import Foundation
---
+++ Elo Book/Views/SubViews/EventChatRoom/ChatMessageDisplayer.swift
@@ -0,0 +1,8 @@
+//
+//  ChatMessageDisplayer.swift
+//  Elo Book
+//
+//  Created by Jed Kutai on 1/14/24.
+//
+
+import Foundation
