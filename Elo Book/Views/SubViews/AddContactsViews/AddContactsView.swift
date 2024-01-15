c194098f16a6f298b74a872323d09c4024ed9acb
Jed <jedkutai@gmail.com>
2024-01-14 22:53:04 -0500

    beginning to allow finding contacts

---
+++ Elo Book/Views/SubViews/AddContactsViews/AddContactsView.swift
@@ -0,0 +1,18 @@
+//
+//  AddContactsView.swift
+//  Elo Book
+//
+//  Created by Jed Kutai on 1/14/24.
+//
+
+import SwiftUI
+
+struct AddContactsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddContactsView()
+}
