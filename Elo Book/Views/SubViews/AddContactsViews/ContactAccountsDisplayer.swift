7de82da1246c01e88333734c5bf9a270226adc14
Jed <jedkutai@gmail.com>
2024-01-16 01:17:06 -0500

    added chatroom and contacts

---
+++ Elo Book/Views/SubViews/AddContactsViews/ContactAccountsDisplayer.swift
@@ -0,0 +1,18 @@
+//
+//  ContactAccountsDisplayer.swift
+//  Elo Book
+//
+//  Created by Jed Kutai on 1/15/24.
+//
+
+import SwiftUI
+
+struct ContactAccountsDisplayer: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ContactAccountsDisplayer()
+}
