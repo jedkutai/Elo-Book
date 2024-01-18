28aadb47aa6d3de14b29271982d8cfd08d622dfa
Jed <jedkutai@gmail.com>
2024-01-18 02:20:18 -0500

    Can leave groups and add users

---
+++ Elo Book/Views/MainTabViews/MessageViews/MessageViewComponents/AddUsersToGroupChatView.swift
@@ -0,0 +1,18 @@
+//
+//  AddUsersToGroupChatView.swift
+//  Elo Book
+//
+//  Created by Jed Kutai on 1/17/24.
+//
+
+import SwiftUI
+
+struct AddUsersToGroupChatView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddUsersToGroupChatView()
+}
---
+++ Elo Book/Views/SubViews/ExpandedPostCelll/ExpandedPostCell.swift
@@ -0,0 +1,18 @@
+//
+//  ExpandedPostCell.swift
+//  Elo Book
+//
+//  Created by Jed Kutai on 1/18/24.
+//
+
+import SwiftUI
+
+struct ExpandedPostCell: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ExpandedPostCell()
+}
