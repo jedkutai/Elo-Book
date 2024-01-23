7cced74853f05514d671322c17e5c80ced3bd516
Jed <jedkutai@gmail.com>
2024-01-23 04:31:27 -0500

    added account deletion

---
+++ Elo Book/Views/SubViews/DeleteAccountViews/DeleteAccountView.swift
@@ -0,0 +1,18 @@
+//
+//  DeleteAccountView.swift
+//  Elo Book
+//
+//  Created by Jed Kutai on 1/23/24.
+//
+
+import SwiftUI
+
+struct DeleteAccountView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DeleteAccountView()
+}
