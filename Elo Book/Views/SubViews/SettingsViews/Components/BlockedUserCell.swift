84c33a4f843fe8e434958d5ef3f5c48d8d2f1b28
Jed <jedkutai@gmail.com>
2024-01-23 14:26:35 -0500

    added blocked users view in settings

---
+++ Elo Book/Views/SubViews/SettingsViews/Components/BlockedUserCell.swift
@@ -0,0 +1,18 @@
+//
+//  BlockedUserCell.swift
+//  Elo Book
+//
+//  Created by Jed Kutai on 1/23/24.
+//
+
+import SwiftUI
+
+struct BlockedUserCell: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BlockedUserCell()
+}
