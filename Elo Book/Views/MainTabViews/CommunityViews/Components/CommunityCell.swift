b0f99ecc404ea0de3781fe40807d8a28bdcf00de
Jed <jedkutai@gmail.com>
2024-02-03 06:30:12 -0500

    login bug

---
+++ Elo Book/Views/MainTabViews/CommunityViews/Components/CommunityCell.swift
@@ -0,0 +1,18 @@
+//
+//  CommunityCell.swift
+//  Elo Book
+//
+//  Created by Jed Kutai on 2/3/24.
+//
+
+import SwiftUI
+
+struct CommunityCell: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CommunityCell()
+}
