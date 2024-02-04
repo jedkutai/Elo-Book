697bef54bd02bb30d41cb80b6d9ccf732b3f32d2
Jed <jedkutai@gmail.com>
2024-02-04 06:25:04 -0500

    small changes

---
+++ Elo Book/Views/MainTabViews/CommunityViews/SingleCommunityView/SingleCommunityViewController.swift
@@ -0,0 +1,18 @@
+//
+//  SingleCommunityViewController.swift
+//  Elo Book
+//
+//  Created by Jed Kutai on 2/3/24.
+//
+
+import SwiftUI
+
+struct SingleCommunityViewController: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SingleCommunityViewController()
+}
